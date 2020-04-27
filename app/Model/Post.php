39ef936f3a73d8f9465ebedadf06c663b82aac16
sayamahendra <60665237+sayamahendra@users.noreply.github.com>
2020-04-27 15:34:23 +0700

    Initial commit

---
+++ app/Model/Post.php
@@ -0,0 +1,50 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Post Model
+ *
+ */
+class Post extends AppModel {
+
+/**
+ * Use database config
+ *
+ * @var string
+ */
+	public $useDbConfig = 'ibntraining';
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'title';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'title' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'body' => array(
+			'notBlank' => array(
+				'rule' => array('notBlank'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
